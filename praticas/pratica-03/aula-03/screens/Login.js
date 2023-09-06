8c8d401d1e4e2833077470c596ea887bd9b6efac
Igor almeida <igor3108almeida@gmail.com>
2023-09-05 21:35:11 -0300

    aula-03

---
+++ praticas/pratica-03/aula-03/screens/Login.js
@@ -0,0 +1,48 @@
+import { View, Text, TextInput, Button, StyleSheet, Image } from 'react-native';
+
+const Login = () => {
+  return (
+    <View style={estilos.container}>
+    <Image style={estilos.logo}
+     source={require('../assets/snack-icon.png')}
+     />
+      <Text style={estilos.titulo}>Login</Text>
+      <TextInput style={estilos.input}
+       placeholder={'E-mail'}
+       keyboardType={'email-address'}
+       />
+       <TextInput style={estilos.input}
+       placeholder={'Senha'}
+       secureTextEntry={true}
+       />
+       <Button 
+       title="Entrar" />
+    </View>
+  );
+};
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+  },
+  logo: {
+  height: 72,
+  width: 72,
+  },
+titulo: {
+fontSize: 28,
+fontWeight: 400,
+textAlign:'center',
+marginBottom: 24,
+},
+input: {
+  lineHeight: 24,
+  borderBottomWidth: 1,
+  borderBottomColor: 'lightgray',
+  margingBottom:16,
+}
+});
+
+export default Login;
